93d2f38a681b571cba141aa95ea3a56950053b27
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-02-11 15:13:26 +0900

    adjust

--- cmd/slack-status-changer/main.go
+++ cmd/slack-status-changer/main.go
@@ -1,33 +1,51 @@
 package main
 
+import (
+	"crypto/tls"
+	"github.com/bookun/slack-status-changer"
+	"github.com/mitchellh/go-homedir"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Manifest struct {
+	WEB    string   `yaml:"internal_webapp_url"`
+	Tokens []string `yaml:"tokens"`
+}
+
 func main() {
-	// slack := changer.Slack{}
-	// homedir, err := homedir.Dir()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// data, err := ioutil.ReadFile(homedir + "/.slack.yml")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// err = yaml.Unmarshal(data, &slack)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// client := &http.Client{
-	// 	Transport: &http.Transport{
-	// 		TLSClientConfig: &tls.Config{
-	// 			InsecureSkipVerify: true,
-	// 		},
-	// 	},
-	// }
-	// resp, err := client.Get(slack.Endpoint)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if resp.StatusCode == 200 {
-	// 	slack.sendStatus("office")
-	// } else {
-	// 	slack.sendStatus("house")
-	// }
+	homedir, err := homedir.Dir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := ioutil.ReadFile(homedir + "/.slack.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var manifest Manifest
+	err = yaml.Unmarshal(data, &manifest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+	config := changer.Config{
+		HTTPClient: httpClient,
+	}
+	slackClient := changer.NewSlackClient(config, manifest.Tokens)
+	command := changer.Command{
+		InternalURL: manifest.WEB,
+		SlackClient: slackClient,
+	}
+	if err := command.Run(); err != nil {
+		log.Fatal(err)
+	}
+	return
 }
